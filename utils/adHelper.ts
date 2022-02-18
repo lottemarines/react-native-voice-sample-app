5ad0cc71e1af32755f58ff971470f646d97b36ef
lottemarines <lottemarines3@gmail.com>
2022-02-18 22:41:17 +0900

    adhelper

--- utils/adHelper.ts
+++ utils/adHelper.ts
@@ -4,8 +4,8 @@
 
 // const AdMobScreenId = Platform.OS === 'ios' ? ENV.AdMobScreenId_iOS : ENV.AdMobScreenId_Android;
 
-// export const Interstitial = async() => {
-//   await AdMobInterstitial.setAdUnitID(AdMobScreenId);
-//   await AdMobInterstitial.requestAdAsync();
-//   await AdMobInterstitial.showAdAsync();
-// }
+export const Interstitial = async () => {
+  //   await AdMobInterstitial.setAdUnitID(AdMobScreenId);
+  //   await AdMobInterstitial.requestAdAsync();
+  //   await AdMobInterstitial.showAdAsync();
+};
