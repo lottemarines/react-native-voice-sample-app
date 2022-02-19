1394e83d1db9b19ac05ae34d06953fc97d2f167a
lottemarines <lottemarines3@gmail.com>
2022-02-19 18:40:20 +0900

    画像削除

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,21 +1,2 @@
-export const PAKA_IMG = require('./pa-ka-.jpg');
-export const QUESTION_IMG = require('./question.png');
-export const USAGI_IMG = require('./usagi.png');
-export const KURO_IMG = require('./crokkusu.jpg');
-export const TABAKO_IMG = require('./tabako.png');
-export const HIGE_IMG = require('./hige.jpg');
-export const HIROYUKANAI_IMG = require('./hiroyukanai.png');
-export const AKABANE_IMG = require('./akabane.jpg');
-export const UMAIBO_IMG = require('./umaibo.jpg');
-export const UNK_IMG = require('./unk.jpg');
-export const KATUMA_IMG = require('./katuma.jpg');
-export const HURUYA_IMG = require('./huruya.jpg');
-export const HIGEOYAZI_IMG = require('./higeoyazi.jpg');
-export const SAIKUN_IMG = require('./saikun.jpg');
-export const SAIKUN_2_IMG = require('./saikun2.jpg');
-export const HIROYUKI_IMG = require('./hiroyuki.jpg');
-export const BEER_IMG = require('./beer.jpg');
-export const RAP_IMG = require('./hiroyukirap.png');
-
 // コピペ
 // export const  = require('./____.jpg');
